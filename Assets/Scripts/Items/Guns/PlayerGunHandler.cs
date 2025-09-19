b63d6f0381c7e33f2c43817879ffc1b198a40c80
= <vgc4321@gmail.com>
2025-09-19 12:23:52 -0500

    fixed patrolling, started gun rework

---
+++ Assets/Scripts/Items/Guns/PlayerGunHandler.cs
@@ -0,0 +1,64 @@
+﻿using EventBus;
+using Player;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Items.Guns
+{
+    public class PlayerGunHandler : MonoBehaviour
+    {
+        private InputManager _inputManager;
+        private Gun _currentGun;
+        private bool _aimToggled;
+        private EventBinding<ItemSwitchedEvent> _itemSwitchedEventBinding;
+        public void Start()
+        {
+            _inputManager = InputManager.Instance;
+            _inputManager.InputActions.Player.Aim.started += OnAim;
+            _itemSwitchedEventBinding = new EventBinding<ItemSwitchedEvent>(OnItemSwitched);
+            EventBus<ItemSwitchedEvent>.Register(_itemSwitchedEventBinding);
+
+        }
+
+        private void OnItemSwitched(ItemSwitchedEvent obj)
+        {
+            if(obj.NewItem is Gun newGun)
+            {
+                _currentGun?.StopAiming();
+                _currentGun = newGun;
+                if(_aimToggled)
+                    _currentGun.StartAiming();
+            }
+            else
+            {
+                _currentGun?.StopAiming();
+                _currentGun = null;
+            }
+        }
+
+        public void OnAim(InputAction.CallbackContext context)
+        {
+            
+            if (context.started)
+            {
+                _aimToggled = !_aimToggled;
+            }
+
+            if (_aimToggled)
+            {
+                _currentGun.StartAiming();
+            }
+            else if (!_aimToggled)
+            {
+                _currentGun. StopAiming();
+            }
+        }
+
+        public void OnReload(InputAction.CallbackContext context)
+        {
+            if (context.started)
+                _currentGun.StartReload();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Items/ItemSwitchedEvent.cs
@@ -0,0 +1,14 @@
+﻿using EventBus;
+
+namespace Items
+{
+    internal class ItemSwitchedEvent : IEvent
+    {
+        public Item NewItem { get; set; }
+        public ItemSwitchedEvent(Item newItem)
+        {
+            NewItem = newItem;
+        }
+    
+    }
+}
